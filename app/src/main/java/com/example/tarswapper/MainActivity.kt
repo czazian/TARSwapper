20931043a5ffb022231594b93d47d09b02117d3e
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-10-25 00:12:58 +0800

    Bottom Navigation Bar and Effects Done

--- app/src/main/java/com/example/tarswapper/MainActivity.kt
+++ app/src/main/java/com/example/tarswapper/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.tarswapper
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tarswapper.Chat
+import com.example.tarswapper.R
+import com.example.tarswapper.Setting
+import com.example.tarswapper.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentManager = supportFragmentManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Set initial fragment
+        if (savedInstanceState == null) {
+            val transaction = fragmentManager.beginTransaction()
+            val initialFragment = Chat()
+            transaction.replace(binding.frameLayout.id, initialFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.video -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Chat()
+                    transaction.replace(binding.frameLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+
+                R.id.community -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Chat()
+                    transaction.replace(binding.frameLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+
+                R.id.tag -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Chat()
+                    transaction.replace(binding.frameLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+
+                R.id.chat -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Chat()
+                    transaction.replace(binding.frameLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+
+                R.id.setting -> {
+                    // After a successful login, show the frameLayout and hide the businessProfileLayout
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Setting()
+                    transaction.replace(binding.frameLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+            }
+            true
+        }
+    }
+    fun restartActivity() {
+        val intent = intent
+        finish()
+        startActivity(intent)
     }
 }
